780fe12f59636f52d0610fe50c03f5d3564163c0
Drew Webster <drewwebs@gmail.com>
2020-09-08 13:11:41 -0400

    Updating to add setNums

--- w10d1/calculator_app/calculator.jsx
+++ w10d1/calculator_app/calculator.jsx
@@ -3,17 +3,21 @@ import React from 'react';
 class Calculator extends React.Component{
   constructor(props){
     super(props);
-    //your code here
+    this.state = {result: 0, num1: "", num2: ""};
+    this.setNum1 = this.setNum1.bind(this);
   }
 
   //your code here
+  setNum1(e) {
+
+  }
 
   render(){
     return (
       <div>
-        <h1>Hello World</h1>
-        //your code will replace this
-      </div>
+        <h1>{this.state.result}</h1>
+        
+      </div>  
     );
   }
 }
